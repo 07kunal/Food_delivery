cfc2a38bdcc9ddcadd3cbedc907f26d50e797e44
kunal <kunalgautam200@gmail.com>
2023-01-12 17:58:46 +0530

    updated:database mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("server...")
+const express = require("express")
+const colors = require("colors")
+const connectDb = require("./config/db")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 5000
+
+// connect to database
+connectDb()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: "Welcome to Pizza_Delivery_app API" })
+})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) 
